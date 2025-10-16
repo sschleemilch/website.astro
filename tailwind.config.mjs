be4a15873f74c83e6669b5c31d973b70cfb0164b
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2025-10-16 08:20:03 +0200

    dev

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  safelist: ["dark:text-sky-500", "dark:text-emerald-500", "dark:text-red-400"],
+  safelist: ["dark:text-sky-500", "dark:text-emerald-500", "dark:text-red-400", "dark:text-fuchsia-500"],
 };
